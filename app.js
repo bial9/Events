8a517ec25504edf5b2e793522a92e9a99040a554
Bilal <99763904+bial9@users.noreply.github.com>
2022-06-01 00:03:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function a (){
+    alert("Thanks for shopping")
+}
+function b (){
+    if (onclick == true) {
+        delete b
+    }
+}
+ function imgChange(toggle){
+        var a = document.getElementById("bulbToggle")
+       if(toggle == 'on'){
+        a.src="img/bulbOn.jpg"
+        }
+        else if(toggle == 'off'){
+        a.src="img/bulbOff.jpg"
+        }
+    }    
